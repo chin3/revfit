089a65ed7aa44d2008359ceff94014bbd77e1799
Patrick Boronski <plboronski@gmail.com>
2020-08-28 17:35:04 -0400

    Fixed project issue

--- src/main/java/com/revature/beans/Role.java
+++ src/main/java/com/revature/beans/Role.java
@@ -1,8 +1,18 @@
 package com.revature.beans;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Role {
 
+	@Id
+	@GeneratedValue
+	@Column(name = "role_id")
 	private int id;
+	
 	private String name;
 	private String description;
 	
