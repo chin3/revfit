c26460386c9e2f6c5cf84002702b2c677c403f71
Adil <adiltruong@umail.ucsb.edu>
2020-08-29 16:20:39 -0700

    add cors bean to app

--- src/main/java/com/revature/main/RevFitApplication.java
+++ src/main/java/com/revature/main/RevFitApplication.java
@@ -3,8 +3,11 @@ package com.revature.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan("com.revature")
@@ -16,4 +19,14 @@ public class RevFitApplication {
 		SpringApplication.run(RevFitApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer configurer(){
+	  return new WebMvcConfigurer(){
+	    @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	      registry.addMapping("/*")
+	          .allowedOrigins("*");
+	    }
+	 };
+	}
 }
