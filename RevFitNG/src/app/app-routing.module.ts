6594488256e7befeb335df74bafaf789106f38be
nate chin <nathannchin@gmail.com>
2020-08-31 16:26:44 -0500

    Added a basic timer for /timer

--- RevFitNG/src/app/app-routing.module.ts
+++ RevFitNG/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginregisterComponent } from './components/loginregister/loginregister.component';
 import { HomeComponent } from './components/home/home.component';
 import { HomedashComponent } from './components/dashboardcomponents/homedash/homedash.component';
+import { WorkouttimerComponent } from './components/workoutcomponents/workouttimer/workouttimer.component';
 
 
 const routes: Routes = [
 {path : 'loginregister', component: LoginregisterComponent},
 {path : '', redirectTo: 'loginregister', pathMatch: 'full'},
 {path : 'home', component: HomeComponent},
-{path: 'dash', component: HomedashComponent, outlet:"dashboard"}
+{path: 'dash', component: HomedashComponent, outlet:"dashboard"},
+{path: 'timer', component: WorkouttimerComponent}
 ];
 
 @NgModule({
